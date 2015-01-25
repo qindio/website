0acdda9b2df0e1b8ee104ff501087e38bf237199
Lorenzo Planas <lorenzo@getharvest.com>
2015-01-25 13:16:48 +0100

    fix

--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require 'sinatra'
 require 'rest_client'
 
-API_KEY = ENV['MAILGUN_API_KEY']
-API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/<your-mailgun-domain>"
+API_KEY       = ENV['MAILGUN_API_KEY']
+SUBDOMAIN     = ENV['MAILGUN_SUBDOMAIN']
+API_URL       = "https://api:#{API_KEY}@api.mailgun.net/v2/#{SUBDOMAIN}"
+QINDIO_INBOX  = "hello@qindio.com"
 
-post '/send' do
+
+post '/messages' do
+  name    = params['name']
+  email   = params['email']
+  subject = "#{name} <#{email}> request through qindio.com"
+  text    = "#{params['subject']}\n#{'='*80}\n\n#{params['message']}"
+
   RestClient.post(API_URL + "/messages", {
-    from:     params['from'],
-    to:       params['to'],
-    subject:  params['subject'],
-    text:     params['body']
+    from:     "web@qindio.com",
+    to:       QINDIO_INBOX,
+    subject:  subject,
+    text:     text
   })
+
+  [201, {}, 'Message sent!']
 end
 
